a50a69f31e9a978d89f5a43cd281e44e0e36bedd
LeilaLaa <aaleila19@gmail.com>
2018-02-13 11:26:40 +0100

    Fin etape 2

--- src/common/talk.service.js
+++ src/common/talk.service.js
@@ -1,18 +1,19 @@
-function speakersGet(url){
-  return new Promise((resolve,reject)=> {
-  const req = new XMLHttpRequest();
-  req.open('GET', 'http://localhost:3000/speakers', true);
-  req.onreadystatechange = (aEvt) => {
-  if (req.readyState == 4) {
-    if(req.status == 200)
-      resolve(JSON.parse(req.reponseText)); // corps de la réponse
-    else
-      reject("Erreur pendant le chargement de la page");
+export class TalkService {
+
+  findAllSpeakers(){
+    return new Promise((resolve,reject)=> {
+    const req = new XMLHttpRequest();
+    req.open('GET', 'http://localhost:3000/speakers', true);
+    req.onreadystatechange = (aEvt) => {
+    if (req.readyState == 4) {
+      if(req.status == 200)
+        resolve(JSON.parse(req.responseText)); // corps de la réponse
+      else
+        reject("Erreur pendant le chargement de la page");
+    }
+    };
+    req.send(null);
+    })
   }
-  };
-  req.send(null);
-  })
-}
-function findAllSpeakers(){
-  return speakersGet();
+
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-//import TalkService from './src/common/talk.service'
-//const ts = new TalkService()
-//ts.findAllSpeakers().then(sps=> console.log(sps.map(sp=>sp.firstname)));
+import {TalkService} from '.\\common\\talk.service'
+const ts = new TalkService()
+
+// On affiche les prenoms des speakers dans la console à l'aide de la fonction findAllSpeakersn de TalkService
+// Cette fonction retourne une promesse de tableau
+ts.findAllSpeakers()
+  .then(sps=> console.log(sps.map(sp=>sp.firstname)));
+
 alert('Conférence App démarré !');
+//ts.findAllSpeakers().then();
