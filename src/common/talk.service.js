baeb49c40db7168a62dea6009e340b7ce933b19f
LeilaLaa <aaleila19@gmail.com>
2018-02-13 09:42:55 +0100

    first commit

---
+++ src/common/talk.service.js
@@ -0,0 +1,18 @@
+function speakersGet(url){
+  return new Promise((resolve,reject)=> {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'http://localhost:3000/speakers', true);
+  req.onreadystatechange = (aEvt) => {
+  if (req.readyState == 4) {
+    if(req.status == 200)
+      resolve(JSON.parse(req.reponseText)); // corps de la réponse
+    else
+      reject("Erreur pendant le chargement de la page");
+  }
+  };
+  req.send(null);
+  })
+}
+function findAllSpeakers(){
+  return speakersGet();
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+//import TalkService from './src/common/talk.service'
+//const ts = new TalkService()
+//ts.findAllSpeakers().then(sps=> console.log(sps.map(sp=>sp.firstname)));
+alert('Conférence App démarré !');
